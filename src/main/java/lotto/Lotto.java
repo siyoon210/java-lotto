7325c05fc0a7776342ba3f3576034a3366d24bb2
siyoon <siyoon210@gmail.com>
2020-11-06 08:15:09 +0900

    feat: Lotto 생성, 최대 범위 숫자와 size를 인자로 받음

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,4 +1,27 @@
 package lotto;
 
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Lotto {
+    private final Random random = new Random();
+    private final Set<Integer> numbers;
+
+    public Lotto() {
+        numbers = new TreeSet<>();
+    }
+
+    public Lotto(int maxNum, int size) {
+        numbers = new TreeSet<>();
+        for (int i = 0; i < size; i++) {
+            while (numbers.size() <= i) {
+                numbers.add(random.nextInt(maxNum + 1));
+            }
+        }
+    }
+
+    public int numbersSize() {
+        return numbers.size();
+    }
 }
