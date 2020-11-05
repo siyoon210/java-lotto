0e85fb465e60c1e18e65b5e2b0db8884c696f8c6
siyoon <siyoon210@gmail.com>
2020-11-06 07:55:02 +0900

    refactor: 입력된 amount로 구입할수 있는 만큼 Lotto 생성

--- src/main/java/lotto/LottoFactory.java
+++ src/main/java/lotto/LottoFactory.java
@@ -1,10 +1,16 @@
 package lotto;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoFactory {
+    public static final int PRICE_OF_ONE_LOTTO = 1000;
+
     public List<Lotto> buyLotto(int amount) {
-        return Arrays.asList(new Lotto(), new Lotto(), new Lotto(), new Lotto(), new Lotto(), new Lotto(), new Lotto(), new Lotto(), new Lotto(), new Lotto());
+        List<Lotto> lottos = new ArrayList<>();
+        for (; amount >= PRICE_OF_ONE_LOTTO; amount -= PRICE_OF_ONE_LOTTO) {
+            lottos.add(new Lotto());
+        }
+        return lottos;
     }
 }
