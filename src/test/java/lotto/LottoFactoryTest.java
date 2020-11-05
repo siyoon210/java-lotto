455164129fd28d94c3863a16af234c8d62681217
siyoon <siyoon210@gmail.com>
2020-11-06 08:01:42 +0900

    test: buyLotto 테스트 케이스 추가
    (경계값 추가)

--- src/test/java/lotto/LottoFactoryTest.java
+++ src/test/java/lotto/LottoFactoryTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class LottoFactoryTest {
     private LottoFactory lottoFactory;
@@ -19,8 +18,13 @@ public class LottoFactoryTest {
     @Test
     @DisplayName("입력된 금액으로 구입할 수 있는 최대 갯수의 로또를 반환한다.")
     void buyLottoTest() {
-        int amount = 10000;
-        List<Lotto> lottos =  lottoFactory.buyLotto(amount);
-        assertThat(lottos.size()).isEqualTo(10);
+        assertAll(
+                () -> assertThat(lottoFactory.buyLotto(-1).size()).isEqualTo(0),
+                () -> assertThat(lottoFactory.buyLotto(0).size()).isEqualTo(0),
+                () -> assertThat(lottoFactory.buyLotto(999).size()).isEqualTo(0),
+                () -> assertThat(lottoFactory.buyLotto(1000).size()).isEqualTo(1),
+                () -> assertThat(lottoFactory.buyLotto(10000).size()).isEqualTo(10),
+                () -> assertThat(lottoFactory.buyLotto(10500).size()).isEqualTo(10)
+        );
     }
 }
