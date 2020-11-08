825225a43742855c83c2eae28efe4bd4307ef670
siyoon <siyoon210@gmail.com>
2020-11-08 12:03:11 +0900

    refactor: 메서드명 rename

--- src/main/java/lotto/LottoFactory.java
+++ src/main/java/lotto/LottoFactory.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class LottoFactory {
     public static final int PRICE_OF_ONE_LOTTO = 1000;
 
-    public List<Lotto> buyLotto(int amount) {
+    public List<Lotto> buyLottos(int amount) {
         List<Lotto> lottos = new ArrayList<>();
         for (; amount >= PRICE_OF_ONE_LOTTO; amount -= PRICE_OF_ONE_LOTTO) {
             lottos.add(new Lotto());
