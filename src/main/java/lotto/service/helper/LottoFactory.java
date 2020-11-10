dd3a7d2d74cabc5fc0e27760bd033f9aefee9503
siyoon <siyoon210@gmail.com>
2020-11-10 22:50:50 +0900

    refacotr: helper클래스 생성자 private로 숨김

--- src/main/java/lotto/service/helper/LottoFactory.java
+++ src/main/java/lotto/service/helper/LottoFactory.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class LottoFactory {
     public static final int PRICE_OF_ONE_LOTTO = 1000;
 
+    private LottoFactory() {
+    }
+
     public static List<Lotto> buyLottos(int amount) {
         List<Lotto> lottos = new ArrayList<>();
         for (; amount >= PRICE_OF_ONE_LOTTO; amount -= PRICE_OF_ONE_LOTTO) {
